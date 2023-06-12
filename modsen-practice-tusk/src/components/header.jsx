a3812c06a61ffb2b4ccabe370cb4f5e57386c397
DeepDunge0n <89940561+DeepDunge0n@users.noreply.github.com>
2023-06-12 20:30:55 +0300

    create app

---
+++ modsen-practice-tusk/src/components/header.jsx
@@ -0,0 +1,51 @@
+import React, {Component} from 'react';
+import styles from './header.module.css'
+import request from 'superagent'
+
+class Header extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            books: [],
+            seachField: ''
+        }
+    }
+
+    searchBook = (e) =>{
+        e.preventDefault();
+        request
+            .get("https://www.googleapis.com/books/v1/volumes")
+            .query({q: this.state.searchField})
+            .then((data)=> {
+                console.log(data);
+            })
+    }
+
+    handleSearch = (e) =>{
+        this.setState({searchField: e.target.value})
+    }
+    render(){
+    return(
+        <div className={styles.header}>
+            <h1>Search for books</h1>
+            <form onSubmit={this.searchBook}
+                className={styles.search} action="">
+                <input
+                    onChange={this.handleSearch} 
+                    type='text' 
+                    placeholder='react'>
+                </input>
+                <button type='submit'><i class='bx bx-search-alt-2'></i></button>
+            </form>
+            <div className={styles.sorting}>
+                <div>Categories</div>
+                <input placeholder='all'></input>
+                <div>Sorting by</div>
+                <input placeholder='relevance'></input>
+            </div>
+        </div>
+
+    )}
+}
+
+export default Header;
